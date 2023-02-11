58c0d335717a26261383f6010325d49a673211f4
Jacob Ibáñez Sánchez <jacobibanez@jacobibanez.com>
2023-02-11 16:11:14 +0100

    Remove old interactable plugin

--- addons/interactable/plugin.gd
+++
@@ -1,9 +0,0 @@
-tool
-extends EditorPlugin
-
-func _enter_tree():
-	add_custom_type("interactable", "Area", preload("interactable.gd"), 
-		preload("Area3D.svg"))
-
-func _exit_tree():
-	remove_custom_type("interactable")
